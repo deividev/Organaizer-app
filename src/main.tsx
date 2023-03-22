abca7f5ced41489b1b04dfb4c4b630b1b364f9a4
David seco <deivid.develop@gmail.com>
2023-03-22 10:20:42 +0100

    Feat: control error deactive geolocation in devicvivice

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/main.scss'
+import './styles/main.scss';
+
+if ( !navigator.geolocation) {
+alert('Tu dispositivo no tiene activa la geolocalizacion');
+throw new Error('Tu dispositivo no tiene activa la geolocalizacion');
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
