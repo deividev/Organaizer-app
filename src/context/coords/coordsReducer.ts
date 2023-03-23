abd0bd3f4a4e4784e68e0ae3325d71d4026bfe38
David seco <deivid.develop@gmail.com>
2023-03-23 09:35:07 +0100

    Fix: bug to initial coords

--- src/context/coords/coordsReducer.ts
+++ src/context/coords/coordsReducer.ts
@@ -1,13 +1,17 @@
 import { Coords, CoordsState } from "./CoordsProvider";
 
-type CoordsAction = { type: "setUserLocation"; payload:  Coords };
+type CoordsAction = { type: "setUserLocation"; payload: Coords };
 
 const gradosARadianes = (grados: number) => {
-  return grados * Math.PI / 180;
+  return (grados * Math.PI) / 180;
 };
 
-const calcularDistanciaEntreDosCoordenadas = 
-  (lat1: number, lon1: number, lat2: number, lon2: number) => {
+const calcularDistanciaEntreDosCoordenadas = (
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) => {
   // Convertir todas las coordenadas a radianes
   lat1 = gradosARadianes(lat1);
   lon1 = gradosARadianes(lon1);
@@ -15,9 +19,13 @@ const calcularDistanciaEntreDosCoordenadas =
   lon2 = gradosARadianes(lon2);
   // Aplicar fórmula
   const RADIO_TIERRA_EN_KILOMETROS = 6371;
-  let diferenciaEntreLongitudes = (lon2 - lon1);
-  let diferenciaEntreLatitudes = (lat2 - lat1);
-  let a = Math.pow(Math.sin(diferenciaEntreLatitudes / 2.0), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(diferenciaEntreLongitudes / 2.0), 2);
+  let diferenciaEntreLongitudes = lon2 - lon1;
+  let diferenciaEntreLatitudes = lat2 - lat1;
+  let a =
+    Math.pow(Math.sin(diferenciaEntreLatitudes / 2.0), 2) +
+    Math.cos(lat1) *
+      Math.cos(lat2) *
+      Math.pow(Math.sin(diferenciaEntreLongitudes / 2.0), 2);
   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return RADIO_TIERRA_EN_KILOMETROS * c;
 };
@@ -28,31 +36,43 @@ export const coordsReducer = (
 ): CoordsState => {
   switch (action.type) {
     case "setUserLocation":
+      let sumDistance: number = 0;
       const ultimateCoords = state.coordsList[state.coordsList?.length - 1];
-      const distanceTraveled = calcularDistanciaEntreDosCoordenadas(ultimateCoords.latitude,
-        ultimateCoords.longitude, action.payload.latitude, action.payload.longitude)
+      if ((ultimateCoords.latitude && ultimateCoords.latitude) !== 0) {
+        const distanceTraveleUltimate = calcularDistanciaEntreDosCoordenadas(
+          ultimateCoords.latitude,
+          ultimateCoords.longitude,
+          action.payload.latitude,
+          action.payload.longitude
+        );
+        sumDistance = state?.distanceTraveled + distanceTraveleUltimate;
+      }
       const updateState = state.coordsList.filter((coords: Coords) => {
         if (coords.latitude !== 0 && coords.longitude !== 0) {
-            return coords
-        } 
-        return
-      })
-      if (ultimateCoords.longitude !== action.payload.longitude && ultimateCoords.latitude !== action.payload.latitude) {
+          return coords;
+        }
+        return;
+      });
+      if (
+        ultimateCoords.longitude !== action.payload.longitude &&
+        ultimateCoords.latitude !== action.payload.latitude
+      ) {
         const payload: Coords[] = [...updateState, action.payload];
+
         return {
           ...state,
           isLoading: false,
           userLocation: action.payload,
           coordsList: payload,
-          distanceTraveled
+          distanceTraveled: sumDistance,
         };
       }
       return {
         ...state,
         isLoading: false,
-        distanceTraveled
+        distanceTraveled: sumDistance,
       };
-      
+
     default:
       return state;
   }
