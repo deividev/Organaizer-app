15a3c0e1ccedaee9f3f8669cc2506cf63d344301
David seco <deivid.develop@gmail.com>
2023-03-23 09:17:14 +0100

    Feat: calculate distance traveled

--- src/context/coords/coordsReducer.ts
+++ src/context/coords/coordsReducer.ts
@@ -2,14 +2,36 @@ import { Coords, CoordsState } from "./CoordsProvider";
 
 type CoordsAction = { type: "setUserLocation"; payload:  Coords };
 
+const gradosARadianes = (grados: number) => {
+  return grados * Math.PI / 180;
+};
+
+const calcularDistanciaEntreDosCoordenadas = 
+  (lat1: number, lon1: number, lat2: number, lon2: number) => {
+  // Convertir todas las coordenadas a radianes
+  lat1 = gradosARadianes(lat1);
+  lon1 = gradosARadianes(lon1);
+  lat2 = gradosARadianes(lat2);
+  lon2 = gradosARadianes(lon2);
+  // Aplicar fórmula
+  const RADIO_TIERRA_EN_KILOMETROS = 6371;
+  let diferenciaEntreLongitudes = (lon2 - lon1);
+  let diferenciaEntreLatitudes = (lat2 - lat1);
+  let a = Math.pow(Math.sin(diferenciaEntreLatitudes / 2.0), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(diferenciaEntreLongitudes / 2.0), 2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return RADIO_TIERRA_EN_KILOMETROS * c;
+};
+
 export const coordsReducer = (
   state: CoordsState,
   action: CoordsAction
 ): CoordsState => {
   switch (action.type) {
     case "setUserLocation":
-      debugger
       const ultimateCoords = state.coordsList[state.coordsList?.length - 1];
+      const distanceTraveled = calcularDistanciaEntreDosCoordenadas(ultimateCoords.latitude,
+        ultimateCoords.longitude, action.payload.latitude, action.payload.longitude)
+      console.log(distanceTraveled);
       const updateState = state.coordsList.filter((coords: Coords) => {
         if (coords.latitude !== 0 && coords.longitude !== 0) {
             return coords
