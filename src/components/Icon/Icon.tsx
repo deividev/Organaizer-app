867e4f39ea3600e82d2d622a522883fc4d633478
David seco <deivid.develop@gmail.com>
2023-03-27 10:02:46 +0200

    Fix: not-display icon for the error list icon exist

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -28,19 +28,24 @@ export const Icon = ({
     icon,
     ...props
   }: IconProps) => {
-  
+
   return (
-    <svg className={props.className} onClick={props.onClick}
-      width={`${props.size ? props.size : '24'}px`}
-      height={`${props.size ? props.size : '24'}px`}
-      viewBox="0 0 24 24"
-    >
-	    <path fill="none" d="M0 0h24v24H0z"/>
-      <path
-		    fill={props.className ? props.className.fill : props.color}
-        d={ICONS[icon]}
-      ></path>
-    </svg>
+    <>
+      {ICONS[icon] && 
+        <svg className={props.className} onClick={props.onClick}
+        width={`${props.size ? props.size : '24'}px`}
+        height={`${props.size ? props.size : '24'}px`}
+        viewBox="0 0 24 24"
+        >
+          <path
+            fill={props.className ? props.className.fill : props.color}
+            d={ICONS[icon]}
+          ></path>
+          <path fill="none" d="M0 0h24v24H0z"/>
+          
+        </svg>
+      }
+    </>
   );
 };
 
