ba058a391c56edb1037fb631e5197e4ff9d627ac
David seco <deivid.develop@gmail.com>
2023-04-03 08:24:32 +0200

    Feat: add loading for the mapView

--- src/components/MapView/MapView.tsx
+++ src/components/MapView/MapView.tsx
@@ -1,61 +1,86 @@
-
-import { useContext, useLayoutEffect, useRef } from "react"
+import { useContext, useLayoutEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
-import { CoordsContext } from "../../context"
+import { CoordsContext } from "../../context";
 import { Coords } from "../../context/coords/CoordsProvider";
-import { Loading } from "../Loading/Loading"
-
+import { Loading } from "../Loading/Loading";
 
 export const MapView = () => {
-    
+  const { isLoading, userLocation, coordsList, distanceTraveled } =
+    useContext(CoordsContext);
+  const mapContainer = useRef<HTMLDivElement>(null);
 
-    const { isLoading, userLocation, coordsList, distanceTraveled } = useContext(CoordsContext);
-    const mapContainer = useRef<HTMLDivElement>(null)
-    
-    useLayoutEffect(() => {
-      if (!isLoading) {
-        const map = new mapboxgl.Map({
-            container: mapContainer.current!, // container ID
-            style: 'mapbox://styles/mapbox/streets-v12', // style URL
-            center: [userLocation?.longitude!, userLocation?.latitude!], // starting position [lng, lat]
-            zoom: 9, // starting zoom
-        });
-      }
-    }, [ isLoading ])
+  useLayoutEffect(() => {
+    if (!isLoading) {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current!, // container ID
+        style: "mapbox://styles/mapbox/streets-v12", // style URL
+        center: [userLocation?.longitude!, userLocation?.latitude!], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+      });
+      // map.on('load', () => {
+      //   map.addSource('mapbox-terrain', {
+      //   type: 'vector',
+      //   // Use any Mapbox-hosted tileset using its tileset id.
+      //   // Learn more about where to find a tileset id:
+      //   // https://docs.mapbox.com/help/glossary/tileset-id/
+      //   url: 'mapbox://mapbox.mapbox-terrain-v2'
+      //   });
+      //   map.addLayer(
+      //   {
+      //   'id': 'terrain-data',
+      //   'type': 'line',
+      //   'source': 'mapbox-terrain',
+      //   'source-layer': 'contour',
+      //   'layout': {
+      //   'line-join': 'round',
+      //   'line-cap': 'round'
+      //   },
+      //   'paint': {
+      //   'line-color': '#ff69b4',
+      //   'line-width': 1
+      //   }
+      //   },
+      //   'road-label-simple' // Add layer below labels
+      //   );
+      //   });
+    }
+  }, [isLoading]);
 
-    if ( isLoading ) {
-        return ( <Loading />)
-    };
-    return (
-        <>
-            <div ref={mapContainer}
-            style={{
-                width: '80vw',
-                height: '80vh',
-                position: 'fixed',
-                top: '0',
-                left: '0',
-            }}>           
+  if (isLoading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <div
+        ref={mapContainer}
+        style={{
+          width: "80vw",
+          height: "80vh",
+          position: "fixed",
+          top: "0",
+          left: "0",
+        }}
+      ></div>
+      <div
+        style={{
+          position: "fixed",
+          bottom: "0",
+          left: "0",
+        }}
+      >
+        CurrentPositon:
+        <span> Longitud: {userLocation?.longitude} </span>
+        <span> Latitud: {userLocation?.latitude}</span>
+        {coordsList?.map((coords: Coords, index) => {
+          return (
+            <div key={index}>
+              <span>Longitud: {coords?.longitude} </span>
+              <span>Latitud: {coords?.latitude}</span>
             </div>
-            <div  style={{
-                
-                position: 'fixed',
-                bottom: '0',
-                left: '0',
-            }}>  
-                CurrentPositon:
-                <span> Longitud: { userLocation?.longitude} </span>
-                <span> Latitud: {userLocation?.latitude}</span>
-                {coordsList?.map((coords: Coords, index) => { 
-                    return (
-                        <div key={index}>
-                            <span>Longitud: {coords?.longitude} </span>
-                            <span>Latitud: {coords?.latitude}</span>
-                        </div>
-                    )
-                })}
-                <p>KM Recorridos: {distanceTraveled}km</p>
-            </div>
-        </>
-    )
-}
+          );
+        })}
+        <p>KM Recorridos: {distanceTraveled}km</p>
+      </div>
+    </>
+  );
+};
